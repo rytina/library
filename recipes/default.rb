5180a8dc1b04ccb0cbcd805f197a9ca37542de45
Andreas Rytina <andreas.rytina@etas.com>
2022-09-18 10:02:05 +0000

    Switch from vagrant to docker

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,9 +10,7 @@ include_recipe 'library::web'
 cookbook_file '/usr/lib/cgi-bin/library' do
   source 'library.rb'
   mode '0755'
-  owner 'vagrant'
-  group 'vagrant'
 end
 
-include_recipe 'library::db_install'
-include_recipe 'library::db_setup'
+# include_recipe 'library::db_install'
+# include_recipe 'library::db_setup'
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,12 +1,10 @@
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+# This is an example test, replace with your own test.
+describe user('root') do
+  it { should exist }
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
+describe port(80) do
   it { should_not be_listening }
 end
